aec0f5b25bf289b57d28f0346c925af50232e4f0
cerresologno <samuelegonnella@yahoo.com>
2025-04-04 10:46:52 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+function toggleDarkMode() {
+    document.body.classList.toggle("dark-mode");
+    if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("darkMode", "enabled");
+    } else {
+        localStorage.setItem("darkMode", "disabled");
+    }
+    updateAllPages(); // Aggiorna tutte le pagine
+}
+
+function updateAllPages(darkModeEnabled) {
+    const iframes = document.querySelectorAll("iframe");
+    iframes.forEach(iframe => {
+        try {
+            iframe.contentWindow.postMessage({ darkMode: darkModeEnabled }, "*");
+        } catch (e) {
+            console.error("Errore nell'invio del messaggio all'iframe:", e);
+        }
+    });
+}
+
+// Verifica se la modalità scura è abilitata all'avvio della pagina
+if (localStorage.getItem("darkMode") === "enabled") {
+    document.body.classList.add("dark-mode");
+}
+
+window.addEventListener("message", event => {
+    if (event.data.darkMode !== undefined) {
+        if (event.data.darkMode) {
+            document.body.classList.add("dark-mode");
+            localStorage.setItem("darkMode", "enabled");
+        } else {
+            document.body.classList.remove("dark-mode");
+            localStorage.setItem("darkMode", "disabled");
+        }
+    }
+});

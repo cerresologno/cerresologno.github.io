e516f686077e037e682d4cf37939f7535970bed1
cerresologno <samuelegonnella@yahoo.com>
2025-04-05 11:11:42 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -40,9 +40,15 @@ window.addEventListener("message", event => {
     }
 });
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/service-worker.js');
-    });
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => {
+        console.log('ServiceWorker registration successful');
+      })
+      .catch(err => {
+        console.log('ServiceWorker registration failed: ', err);
+      });
+  });
 }
 
 

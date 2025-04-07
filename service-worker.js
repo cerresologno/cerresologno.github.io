3d9116a0ccc6f7ea23b6549b70cd9ed6a0fa540f
cerresologno <samuelegonnella@yahoo.com>
2025-04-07 11:52:46 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,132 +1,72 @@
-<<<<<<< HEAD
-const cacheName = 'proloco-cerre-sologno-v1';
-const staticAssets = [
-  './',
-  './index.html',  // Add index.html
-  './home.html',
-  './news.html',
-  './eventi.html',
-  './contatti.html',
-  './dove.html',
-  './storia.html',
-  './evento1.html',
-  './evento2.html',
-  './style.css',
-  './script.js',
-  './img/pietra.jpg',
-  './img/icon-192x192.png',
-  './img/icon-512x512.png',
-  // Add common file types
-  './manifest.json'
+const CACHE_NAME = 'cerresologno-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/static/css/main.chunk.css',
+  '/static/js/main.chunk.js',
+  '/static/js/0.chunk.js',
+  '/static/js/bundle.js',
+  '/manifest.json',
+  '/favicon.ico',
+  '/logo192.png',
+  '/logo512.png',
+  // Aggiungi qui altri file specifici del tuo sito
+  '/assets/images/hero.jpg',
+  '/assets/fonts/main-font.woff2'
 ];
 
-self.addEventListener('install', async event => {
-  const cache = await caches.open(cacheName);
-  await cache.addAll(staticAssets);
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
 self.addEventListener('fetch', event => {
-  const req = event.request;
-  const url = new URL(req.url);
-
-  if (url.origin === location.origin) {
-    event.respondWith(cacheFirst(req));
-  } else {
-    event.respondWith(networkFirst(req));
-  }
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        
+        // Clone the request because it's a one-time use
+        return fetch(event.request.clone())
+          .then(response => {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            
+            // Clone the response because it's a one-time use
+            const responseToCache = response.clone();
+            
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+              
+            return response;
+          });
+      })
+    );
 });
 
-async function cacheFirst(req) {
-  const cachedResponse = await caches.match(req);
-  return cachedResponse || networkFirst(req);
-}
-
-async function networkFirst(req) {
-  const cache = await caches.open(cacheName);
-  try {
-    const fresh = await fetch(req);
-    await cache.put(req, fresh.clone());
-    return fresh;
-  } catch (e) {
-    const cachedResponse = await caches.match(req);
-    return cachedResponse;
-  }
-}
-
-// Add activate event handler
 self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
-    caches.keys().then(keys => {
+    caches.keys().then(cacheNames => {
       return Promise.all(
-        keys.filter(key => key !== cacheName)
-          .map(key => caches.delete(key))
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
       );
     })
   );
-=======
-const cacheName = 'proloco-cerre-sologno-v1';
-const staticAssets = [
-  './',
-  './index.html',  // Add index.html
-  './home.html',
-  './news.html',
-  './eventi.html',
-  './contatti.html',
-  './dove.html',
-  './storia.html',
-  './evento1.html',
-  './evento2.html',
-  './style.css',
-  './script.js',
-  './img/pietra.jpg',
-  './img/icon-192x192.png',
-  './img/icon-512x512.png',
-  // Add common file types
-  './manifest.json'
-];
-
-self.addEventListener('install', async event => {
-  const cache = await caches.open(cacheName);
-  await cache.addAll(staticAssets);
-});
-
-self.addEventListener('fetch', event => {
-  const req = event.request;
-  const url = new URL(req.url);
-
-  if (url.origin === location.origin) {
-    event.respondWith(cacheFirst(req));
-  } else {
-    event.respondWith(networkFirst(req));
-  }
-});
-
-async function cacheFirst(req) {
-  const cachedResponse = await caches.match(req);
-  return cachedResponse || networkFirst(req);
-}
-
-async function networkFirst(req) {
-  const cache = await caches.open(cacheName);
-  try {
-    const fresh = await fetch(req);
-    await cache.put(req, fresh.clone());
-    return fresh;
-  } catch (e) {
-    const cachedResponse = await caches.match(req);
-    return cachedResponse;
-  }
-}
-
-// Add activate event handler
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(key => key !== cacheName)
-          .map(key => caches.delete(key))
-      );
-    })
-  );
->>>>>>> 8f76def3e1a617694796d3e66887dfb3a91a4a87
 });
